9a5dfa7dafd00b735f2e5a73878c081315550db2
Patryk <pkazmierczak@sigma.ug.edu.pl>
2019-10-09 21:46:23 +0200

    Another commit

--- e_power_x.py
+++ e_power_x.py
@@ -6,44 +6,51 @@ class EPowerX:
         self.precision = precision
         self.x = x
         self.diff_from_buildin_func = diff
-        self.def_partial_sums = [] # zapisane aby dwa razy nie liczyć tego samego
-        self.rec_partial_sums = [] # zapisane aby dwa razy nie liczyć tego samego
+        self.elements_from_definition = [] # zapisane aby dwa razy nie liczyć tego samego
+        self.elements_from_recursion = [] # zapisane aby dwa razy nie liczyć tego samego
         self.build_in_result = None # zapisane aby dwa razy nie liczyć tego samego
 
-    def func(self, x, n):  # TODO replace build in functions
-        return (x ** n) / (math.factorial(n))
+    def pow(self, x, n):
+        result = 1
+        for i in range(n):
+            result *= x
+
+        return result
+
+    def func(self, x, n):
+        return self.pow(x, n) / (math.factorial(n))
 
     def _is_good_precision(self, elem, n):
         # To tylko na teraz jest tak zrobione bo wyniki są podobne jak przy ograniczeniu z góry
         return self.precision > elem
 
-    def get_partial_sums_from_definition(self):
-        if not self.def_partial_sums:
+    def get_elems_from_definition(self):
+        if not self.elements_from_definition:
             elem = 1
             n = 0
             while not self._is_good_precision(elem, n):
-                self.def_partial_sums.append(elem)
+                self.elements_from_definition.append(elem)
                 n += 1
                 elem = self.func(self.x, n)
 
-        return self.def_partial_sums
+        return self.elements_from_definition
 
-    def get_partial_from_recursion(self):
-        if len(self.rec_partial_sums) <= 1:
+    def get_elems_from_recursion(self):
+        if len(self.elements_from_recursion) <= 1:
             elem = 1
             n = 0
             while not self._is_good_precision(elem, n):
-                self.rec_partial_sums.append(elem)
+                self.elements_from_recursion.append(elem)
                 n += 1
-                elem *= self.x / (n + 1)
+                elem *= self.x / n
 
-        return self.rec_partial_sums
+        return self.elements_from_recursion
 
     def compute_exp(self, method="def", reverse=False):
         if method == "def":
-            series = self.get_partial_sums_from_definition()
+            series = self.get_elems_from_definition()
         else:
-            series = self.get_partial_from_recursion()
+            series = self.get_elems_from_recursion()
 
         if reverse:
             series.reverse()
